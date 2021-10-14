8521150ac0083300620d29e67a57ab955e3eb0e2
Lindsay Lopez <lopez95@berkeley.edu>
2021-10-13 17:48:08 -0700

    started on PersonalInformationInput component

--- src/components/PersonalInformationInput.js
+++ src/components/PersonalInformationInput.js
@@ -1,6 +1,48 @@
+import WebsiteInput from "./WebsiteInput";
+
 function PersonalInformationInput() {
   return (
     <div>
+      <h2>Personal Information</h2>
+      <input 
+        placeholder="First Name"
+        type="text"
+      />
+      <input 
+        placeholder="Last Name"
+        type="text"
+      />
+      <input 
+        placeholder="Title"
+        type="text"
+      />
+      <textarea
+        placeholder="Description"
+        type="text"
+      ></textarea>
+      <div>
+        <label for="photo">Choose a Photo to Upload</label>
+        <input 
+          id="photo"
+          name="photo"
+          placeholder="Title"
+          type="file"
+          accept=".jpg, .jpeg, .png"
+        />
+      </div>
+      <input 
+        placeholder="Location"
+        type="text"
+      />
+      <input 
+        placeholder="Phone Number"
+        type="text"
+      />
+      <input 
+        placeholder="Email"
+        type="text"
+      />
+      <WebsiteInput />
     </div>
   );
 }
---
+++ src/components/WebsiteInput.js
@@ -0,0 +1,18 @@
+function WebsiteInput() {
+  return (
+    <div>
+      <h3>Add a Website</h3>
+      <input 
+        placeholder="Title"
+        type="text"
+      />
+      <input 
+        placeholder="Link"
+        type="text"
+      />
+      
+    </div>
+  );
+}
+
+export default WebsiteInput;
