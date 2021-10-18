d12e8af131d1d348cc477cc741f8cfb77c09a209
Lindsay Lopez <lopez95@berkeley.edu>
2021-10-18 07:57:16 -0700

    started experience display, not yet styled

--- src/components/ExperienceDisplay.js
+++ src/components/ExperienceDisplay.js
@@ -1,6 +1,28 @@
-function ExperienceDisplay() {
+function ExperienceDisplay(props) {
+  const { experiences } = props.data;
   return (
-    <div>
+    <div className="experienceDisplay">
+      <h2>Experience</h2>
+      {experiences.map((experience) => {
+        const { 
+          position,
+          company,
+          location,
+          from,
+          to,
+          description,
+         } = experience;
+        return (
+          <div className="experience">
+            <p className="experience__position">{position}</p>
+            <p className="experience__company">{company}</p>
+            <p className="experience__location">{location}</p>
+            <p className="experience__from">{from}</p>
+            <p className="experience__to">{to}</p>
+            <p className="experience__subject">{description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
