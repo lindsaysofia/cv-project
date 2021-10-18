0583192447450d8c3bf102e795b73019ad679283
Lindsay Lopez <lopez95@berkeley.edu>
2021-10-18 07:53:02 -0700

    started EducationDisplay, not yet styled

--- src/components/EducationDisplay.js
+++ src/components/EducationDisplay.js
@@ -1,6 +1,28 @@
-function EducationDisplay() {
+function EducationDisplay(props) {
+  const { educations } = props.data;
   return (
-    <div>
+    <div className="educationDisplay">
+      <h2>Education</h2>
+      {educations.map((education) => {
+        const { 
+          university,
+          location,
+          degree,
+          subject,
+          from,
+          to,
+         } = education;
+        return (
+          <div className="education">
+            <p className="education__university">{university}</p>
+            <p className="education__location">{location}</p>
+            <p className="education__degree">{degree}</p>
+            <p className="education__subject">{subject}</p>
+            <p className="education__from">{from}</p>
+            <p className="education__to">{to}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
