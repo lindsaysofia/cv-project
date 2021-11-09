33501b85bb81fb397b85f68acdaab099392e3f58
Lindsay Lopez <lopez95@berkeley.edu>
2021-11-08 18:23:53 -0800

    changed App to use hooks. Modified handleChange to reflect hook

--- src/App.js
+++ src/App.js
@@ -1,160 +1,481 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import CVInput from './components/CVInput';
 import CVDisplay from './components/CVDisplay';
 import html2pdf from 'html2pdf.js';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.exampleData = {
-      personalInformation: {
-        firstName: 'Bob',
-        lastName: 'the Builder',
-        title: 'Construction Worker',
-        description: 'Can we fix it? Yes, we can!',
-        location: 'Fixham Harbour',
-        phone: '123-456-7890',
-        email: 'bob@icanfixthat.com',
-        websites: [
-        {
-          websiteName: 'github',
-          websiteLink: 'https://github.com/bobTheBuilder',
-        },
-        {
-          websiteName: 'github',
-          websiteLink: 'https://github.com/bobTheBuilder',
-        }
-      ],
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.exampleData = {
+//       personalInformation: {
+//         firstName: 'Bob',
+//         lastName: 'the Builder',
+//         title: 'Construction Worker',
+//         description: 'Can we fix it? Yes, we can!',
+//         location: 'Fixham Harbour',
+//         phone: '123-456-7890',
+//         email: 'bob@icanfixthat.com',
+//         websites: [
+//         {
+//           websiteName: 'github',
+//           websiteLink: 'https://github.com/bobTheBuilder',
+//         },
+//         {
+//           websiteName: 'github',
+//           websiteLink: 'https://github.com/bobTheBuilder',
+//         }
+//       ],
+//       },
+//       skills: ['Problem-Solving', 'A Can-Do Attitude'],
+//       educations: [
+//         {
+//           university: 'University of Bobsville',
+//           location: 'Bobsville, United Kingdom',
+//           degree: 'B.A.',
+//           subject: 'Construction',
+//           from: '1997',
+//           to: 'Present',
+//         },
+//         {
+//           university: 'University of Bobsville',
+//           location: 'Bobsville, United Kingdom',
+//           degree: 'B.A.',
+//           subject: 'Construction',
+//           from: '1997',
+//           to: 'Present',
+//         }
+//       ],
+//       experiences: [
+//         {
+//           position: 'Owner',
+//           company: `Bob's Yard`,
+//           location: 'Bobsville, United Kingdom',
+//           from: '1997',
+//           to: '2007',
+//           description: 'Built and maintained the yard with my father, Robert, and the Can-Do Crew',
+//         },
+//         {
+//           position: 'Owner',
+//           company: `Bob's Yard`,
+//           location: 'Bobsville, United Kingdom',
+//           from: '1997',
+//           to: '2007',
+//           description: 'Built and maintained the yard with my father, Robert, and the Can-Do Crew',
+//         }
+//       ],
+//     };
+//     this.resetData = {
+//       personalInformation: {
+//         firstName: '',
+//         lastName: '',
+//         title: '',
+//         description: '',
+//         location: '',
+//         phone: '',
+//         email: '',
+//         websites: [
+//           {
+//             websiteName: '',
+//             websiteLink: '',
+//           }
+//         ],
+//       },
+//       skills: [''],
+//       educations: [
+//         {
+//           university: '',
+//           location: '',
+//           degree: '',
+//           subject: '',
+//           from: '',
+//           to: '',
+//         },
+//       ],
+//       experiences: [
+//         {
+//           position: '',
+//           company: ``,
+//           location: '',
+//           from: '',
+//           to: '',
+//           description: '',
+//         },
+//       ],
+//     };
+//     this.state = this.resetData;
+//   };
+
+//   handleChange = (e) => {
+//     const { name, value } = e.target;
+//     const { parent, index } = e.target.dataset;
+//     switch (parent) {
+//       case 'personalInformation':
+//         this.setState((prevState) => {
+//           return {
+//             personalInformation: {
+//               ...prevState.personalInformation,
+//               [name] : value,
+//             }
+//           }
+//         });
+//         break;
+//       case 'websites':
+//         this.setState((prevState) => {
+//           const websitesCopy = prevState.personalInformation.websites.slice();
+//           websitesCopy[index][name] = value;
+//           return {
+//             personalInformation: {
+//               ...prevState.personalInformation,
+//               websites: websitesCopy,
+//             }
+//           }
+//         });
+//         break;
+//       case 'skills':
+//         this.setState((prevState) => {
+//           const skillsCopy = prevState.skills.slice();
+//           skillsCopy[index] = value;
+//           return {
+//             skills: skillsCopy,
+//           }
+//         });
+//         break;
+//       case 'educations':
+//         this.setState((prevState) => {
+//           const educationsCopy = prevState.educations.slice();
+//           educationsCopy[index][name] = value;
+//           return {
+//             educations: educationsCopy,
+//           }
+//         });
+//         break;
+//       case 'experiences':
+//         this.setState((prevState) => {
+//           const experiencesCopy = prevState.experiences.slice();
+//           experiencesCopy[index][name] = value;
+//           return {
+//             experiences: experiencesCopy,
+//           }
+//         });
+//         break;
+//       default:
+//         break;
+//     }
+//   };
+
+//   handleAdd = (e) => {
+//     const { parent } = e.target.dataset;
+//     switch (parent) {
+//       case 'websites':
+//         this.setState((prevState) => {
+//           const websitesCopy = prevState.personalInformation.websites.slice();
+//           websitesCopy.push({
+//             websiteName: '',
+//             websiteLink: '',
+//           });
+//           return {
+//             personalInformation: {
+//               ...prevState.personalInformation,
+//               websites: websitesCopy,
+//             }
+//           }
+//         });
+//         break;
+//       case 'skills':
+//         this.setState((prevState) => {
+//           const skillsCopy = prevState.skills.slice();
+//           skillsCopy.push('');
+//           return {
+//             skills: skillsCopy,
+//           }
+//         });
+//         break;
+//       case 'educations':
+//         this.setState((prevState) => {
+//           const educationsCopy = prevState.educations.slice();
+//           educationsCopy.push({
+//             university: '',
+//             location: '',
+//             degree: '',
+//             subject: '',
+//             from: '',
+//             to: '',
+//           });
+//           return {
+//             educations: educationsCopy,
+//           }
+//         });
+//           break;
+//       case 'experiences':
+//         this.setState((prevState) => {
+//           const experiencesCopy = prevState.experiences.slice();
+//           experiencesCopy.push({
+//             position: '',
+//             company: ``,
+//             location: '',
+//             from: '',
+//             to: '',
+//             description: '',
+//           });
+//           return {
+//             experiences: experiencesCopy,
+//           }
+//         });
+//           break;
+//       default:
+//         break;
+//     }
+//   };
+
+//   handleDelete = (e) => {
+//     const { parent, index } = e.target.dataset;
+//     switch (parent) {
+//       case 'websites':
+//         this.setState((prevState) => {
+//           const websitesCopy = prevState.personalInformation.websites.filter((website, websiteIndex) => websiteIndex !== Number(index));
+//           return {
+//             personalInformation: {
+//               ...prevState.personalInformation,
+//               websites: websitesCopy,
+//             }
+//           }
+//         });
+//         break;
+//       case 'skills':
+//         this.setState((prevState) => {
+//           const skillsCopy = prevState.skills.filter((skill, skillIndex) => skillIndex !== Number(index));
+//           return {
+//             skills: skillsCopy,
+//           }
+//         });
+//         break;
+//       case 'educations':
+//         this.setState((prevState) => {
+//           const educationsCopy = prevState.educations.filter((education,educationIndex) =>educationIndex !== Number(index));
+//           return {
+//             educations: educationsCopy,
+//           }
+//         });
+//         break;
+//       case 'experiences':
+//         this.setState((prevState) => {
+//           const experiencesCopy = prevState.experiences.filter((experience,experienceIndex) => experienceIndex !== Number(index));
+//           return {
+//             experiences: experiencesCopy,
+//           }
+//         });
+//         break;
+//       default:
+//         break;
+//     }
+//   };
+
+//   handleExample = () => {
+//     this.setState(this.exampleData);
+//   };
+
+//   handleReset = () => {
+//     this.setState(this.resetData);
+//   };
+
+//   generatePDF = () => {
+//     const element = document.getElementById('pdf');
+//     html2pdf(element);
+//   };
+
+//   // from https://stackoverflow.com/questions/5802580/html-input-type-file-get-the-image-before-submitting-the-form/34840295
+
+//   uploadPhoto = () => {
+//     let preview = document.querySelector('img');
+//     let file = document.querySelector('input[type=file]').files[0];
+//     let reader = new FileReader();
+  
+//     reader.onloadend = function () {
+//       preview.src = reader.result;
+//     }
+  
+//     if (file) {
+//       reader.readAsDataURL(file);
+//     } else {
+//       preview.src = "";
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className="app">
+//         <CVInput 
+//           data={this.state} 
+//           handleChange={this.handleChange}
+//           handleAdd={this.handleAdd}
+//           handleDelete={this.handleDelete}
+//           handleExample={this.handleExample}
+//           handleReset={this.handleReset}
+//           generatePDF={this.generatePDF}
+//           uploadPhoto={this.uploadPhoto}
+//         />
+//         <CVDisplay data={this.state}/>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  const exampleData = {
+    personalInformation: {
+      firstName: 'Bob',
+      lastName: 'the Builder',
+      title: 'Construction Worker',
+      description: 'Can we fix it? Yes, we can!',
+      location: 'Fixham Harbour',
+      phone: '123-456-7890',
+      email: 'bob@icanfixthat.com',
+      websites: [
+      {
+        websiteName: 'github',
+        websiteLink: 'https://github.com/bobTheBuilder',
       },
-      skills: ['Problem-Solving', 'A Can-Do Attitude'],
-      educations: [
-        {
-          university: 'University of Bobsville',
-          location: 'Bobsville, United Kingdom',
-          degree: 'B.A.',
-          subject: 'Construction',
-          from: '1997',
-          to: 'Present',
-        },
-        {
-          university: 'University of Bobsville',
-          location: 'Bobsville, United Kingdom',
-          degree: 'B.A.',
-          subject: 'Construction',
-          from: '1997',
-          to: 'Present',
-        }
-      ],
-      experiences: [
-        {
-          position: 'Owner',
-          company: `Bob's Yard`,
-          location: 'Bobsville, United Kingdom',
-          from: '1997',
-          to: '2007',
-          description: 'Built and maintained the yard with my father, Robert, and the Can-Do Crew',
-        },
-        {
-          position: 'Owner',
-          company: `Bob's Yard`,
-          location: 'Bobsville, United Kingdom',
-          from: '1997',
-          to: '2007',
-          description: 'Built and maintained the yard with my father, Robert, and the Can-Do Crew',
-        }
-      ],
-    };
-    this.resetData = {
-      personalInformation: {
-        firstName: '',
-        lastName: '',
-        title: '',
-        description: '',
-        location: '',
-        phone: '',
-        email: '',
-        websites: [
-          {
-            websiteName: '',
-            websiteLink: '',
-          }
-        ],
+      {
+        websiteName: 'github',
+        websiteLink: 'https://github.com/bobTheBuilder',
+      }
+    ],
+    },
+    skills: ['Problem-Solving', 'A Can-Do Attitude'],
+    educations: [
+      {
+        university: 'University of Bobsville',
+        location: 'Bobsville, United Kingdom',
+        degree: 'B.A.',
+        subject: 'Construction',
+        from: '1997',
+        to: 'Present',
       },
-      skills: [''],
-      educations: [
-        {
-          university: '',
-          location: '',
-          degree: '',
-          subject: '',
-          from: '',
-          to: '',
-        },
-      ],
-      experiences: [
-        {
-          position: '',
-          company: ``,
-          location: '',
-          from: '',
-          to: '',
-          description: '',
-        },
-      ],
-    };
-    this.state = this.resetData;
+      {
+        university: 'University of Bobsville',
+        location: 'Bobsville, United Kingdom',
+        degree: 'B.A.',
+        subject: 'Construction',
+        from: '1997',
+        to: 'Present',
+      }
+    ],
+    experiences: [
+      {
+        position: 'Owner',
+        company: `Bob's Yard`,
+        location: 'Bobsville, United Kingdom',
+        from: '1997',
+        to: '2007',
+        description: 'Built and maintained the yard with my father, Robert, and the Can-Do Crew',
+      },
+      {
+        position: 'Owner',
+        company: `Bob's Yard`,
+        location: 'Bobsville, United Kingdom',
+        from: '1997',
+        to: '2007',
+        description: 'Built and maintained the yard with my father, Robert, and the Can-Do Crew',
+      }
+    ],
   };
 
-  handleChange = (e) => {
+  const resetData = {
+    personalInformation: {
+      firstName: '',
+      lastName: '',
+      title: '',
+      description: '',
+      location: '',
+      phone: '',
+      email: '',
+      websites: [
+        {
+          websiteName: '',
+          websiteLink: '',
+        }
+      ],
+    },
+    skills: [''],
+    educations: [
+      {
+        university: '',
+        location: '',
+        degree: '',
+        subject: '',
+        from: '',
+        to: '',
+      },
+    ],
+    experiences: [
+      {
+        position: '',
+        company: ``,
+        location: '',
+        from: '',
+        to: '',
+        description: '',
+      },
+    ],
+  };
+
+  const [data, setData] = useState(resetData);
+
+  const handleChange = (e) => {
     const { name, value } = e.target;
     const { parent, index } = e.target.dataset;
     switch (parent) {
       case 'personalInformation':
-        this.setState((prevState) => {
+        setData((prevData) => {
           return {
+            ...prevData,
             personalInformation: {
-              ...prevState.personalInformation,
-              [name] : value,
+              ...prevData.personalInformation,
+              [name]: value,
             }
           }
-        });
+        })
         break;
       case 'websites':
-        this.setState((prevState) => {
-          const websitesCopy = prevState.personalInformation.websites.slice();
+        setData((prevData => {
+          const websitesCopy = prevData.personalInformation.websites.slice();
           websitesCopy[index][name] = value;
           return {
+            ...prevData,
             personalInformation: {
-              ...prevState.personalInformation,
+              ...prevData.personalInformation,
               websites: websitesCopy,
             }
           }
-        });
+        }));
         break;
       case 'skills':
-        this.setState((prevState) => {
-          const skillsCopy = prevState.skills.slice();
+        setData((prevData) => {
+          const skillsCopy = prevData.skills.slice();
           skillsCopy[index] = value;
           return {
+            ...prevData,
             skills: skillsCopy,
           }
         });
         break;
       case 'educations':
-        this.setState((prevState) => {
-          const educationsCopy = prevState.educations.slice();
+        setData((prevData) => {
+          const educationsCopy = prevData.educations.slice();
           educationsCopy[index][name] = value;
           return {
+            ...prevData,
             educations: educationsCopy,
           }
         });
         break;
       case 'experiences':
-        this.setState((prevState) => {
-          const experiencesCopy = prevState.experiences.slice();
+        setData((prevData) => {
+          const experiencesCopy = prevData.experiences.slice();
           experiencesCopy[index][name] = value;
           return {
+            ...prevData,
             experiences: experiencesCopy,
           }
         });
@@ -164,7 +485,7 @@ class App extends Component {
     }
   };
 
-  handleAdd = (e) => {
+  const handleAdd = (e) => {
     const { parent } = e.target.dataset;
     switch (parent) {
       case 'websites':
@@ -228,7 +549,7 @@ class App extends Component {
     }
   };
 
-  handleDelete = (e) => {
+  const handleDelete = (e) => {
     const { parent, index } = e.target.dataset;
     switch (parent) {
       case 'websites':
@@ -271,22 +592,22 @@ class App extends Component {
     }
   };
 
-  handleExample = () => {
+  const handleExample = () => {
     this.setState(this.exampleData);
   };
 
-  handleReset = () => {
+  const handleReset = () => {
     this.setState(this.resetData);
   };
 
-  generatePDF = () => {
+  const generatePDF = () => {
     const element = document.getElementById('pdf');
     html2pdf(element);
   };
 
   // from https://stackoverflow.com/questions/5802580/html-input-type-file-get-the-image-before-submitting-the-form/34840295
 
-  uploadPhoto = () => {
+  const uploadPhoto = () => {
     let preview = document.querySelector('img');
     let file = document.querySelector('input[type=file]').files[0];
     let reader = new FileReader();
@@ -301,24 +622,21 @@ class App extends Component {
       preview.src = "";
     }
   };
-
-  render() {
-    return (
-      <div className="app">
-        <CVInput 
-          data={this.state} 
-          handleChange={this.handleChange}
-          handleAdd={this.handleAdd}
-          handleDelete={this.handleDelete}
-          handleExample={this.handleExample}
-          handleReset={this.handleReset}
-          generatePDF={this.generatePDF}
-          uploadPhoto={this.uploadPhoto}
-        />
-        <CVDisplay data={this.state}/>
-      </div>
-    );
-  }
+  return (
+    <div className="app">
+      <CVInput 
+        data={data} 
+        handleChange={handleChange}
+        handleAdd={handleAdd}
+        handleDelete={handleDelete}
+        handleExample={handleExample}
+        handleReset={handleReset}
+        generatePDF={generatePDF}
+        uploadPhoto={uploadPhoto}
+      />
+      <CVDisplay data={data}/>
+    </div>
+  );
 }
 
 export default App;
